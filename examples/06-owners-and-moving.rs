43ce7fda6c39730e09b550bde8cbe5b22970ffbb
Andrei Stefan <A.Stefan-1@student.tudelft.nl>
2023-09-29 17:31:33 +0200

    5-7

--- examples/06-owners-and-moving.rs
+++ examples/06-owners-and-moving.rs
@@ -9,19 +9,20 @@ fn main() {
     //println!("s outside = {}", s); // -> error[E0425]: cannot find value `s` in this scope
 
     // Create a String, then move it into a scope and print it. Observe that the String is no longer accessible after the scope ends.
-    let s = "Mama m-a facut sa ma duc la piata";
+    let s: String = String::from("Mama m-a facut sa ma duc la piata");
     {
         // Move `s` into a variable inside the scope
         let inner_s = s;
 
+        // error[E0382]: borrow of moved value: `s`
         // `s` is no longer accessible here, print inner_s instead
-        println!("s = {}", s);
+        println!("s = {}", inner_s);
     }
 
     // Try to print `s` here.
 
     // Declare an integer, then "move" it into a scope and print it. Observe that the integer is still accessible after the scope ends.
-    // let x = ...
+    let x = 123;
     {
         // "Move" `x` into a variable inside the scope
         let inner_x = x;
@@ -30,10 +31,10 @@ fn main() {
     // `x` is still accessible here, try to print it.
 
     // Create a String, then clone it and move the clone into a scope and print it. Observe that the String is still accessible after the scope ends.
-    // let s = ...
+    let s = String::from("Hello");
     {
         // Clone `s` and move the clone into a variable inside the scope
-        // let inner_s = ...
+        let inner_s = s.clone();
         println!("inner_s = {}", inner_s);
     }
     // `s` is still accessible here
@@ -42,17 +43,21 @@ fn main() {
     // Create a String, then pass it to a function and print it. Observe that the String is no longer accessible after the function call.
 
     // Create a String, then pass it to a function and print the returned String. Observe that the String is still accessible after the function call.
-
+    let s = take_ownership_and_give_it_back(s);
+    println!("{}", s);
     // Create a String, drop it with drop(), the try to print it. Observe that the String is no longer accessible after the drop() call.
+    let st = String::from("Hello");
+    drop(st);
+    //println!("st = {}", st); // -> error[E0382]: borrow of moved value: `st`
 }
 
 // Write a function that takes ownership of a String and prints it.
-fn take_ownership(/* ... */) {
-    // ...
+fn take_ownership(s: String) {
+    println!("s = {}", s);
 } // <- argument goes out of scope and the String is dropped.
 
 // Write a function that takes ownership of a String, appends a word to it, and returns it. Don’t forget to mark the parameter as mutable, like you do with a variable.
-fn take_ownership_and_give_it_back(/* ... */) /* -> ... */
-{
-    // ...
+fn take_ownership_and_give_it_back(mut s: String) -> String {
+    s.push_str(" World!");
+    s
 }
--- examples/07-borrows.rs
+++ examples/07-borrows.rs
@@ -1,33 +1,43 @@
+use core::num;
+
 // 1. Write a function that takes ownership of a String, then counts the number of words in the String and returns the number of words.
-// fn take_ownership_and_count
+fn take_ownership_and_count(s: String) -> usize {
+    let count: usize = s.split_whitespace().count();
+    count
+}
 
 // 3. Write a function that takes ownership of a String, splits it into words and returns a tuple with the String and the number of words.
-// fn take_ownership_count_then_give_back
+fn take_ownership_count_then_give_back(s: String) -> (String, usize) {
+    let count: usize = s.split_whitespace().count();
+    (s, count)
+}
 
 // 5. Write a function that takes a reference to a String, splits it into words and returns the number of words.
-// fn take_reference_and_count
+fn take_reference_and_count(s: &String) -> usize {
+    s.split_whitespace().count()
+}
 
 fn main() {
     let s = String::from("Hello there crab");
 
     // 2. Clone `s` and count words with `take_ownership_and_count()`
-
+    let num_words = take_ownership_and_count(s.clone());
     println!("{} has {} words", s, num_words);
 
-    // 4. Count words with `take_ownership_and_count_and_give_back()`
-
+    // 4. Count words with `take_ownership_and_count_then_give_back()`
+    let (s, num_words) = take_ownership_count_then_give_back(s);
     println!("{} has {} words", s, num_words);
 
     // Create a scope with a String, the create a reference to it and try to return it from the scope
     let s_ref = {
         let s = String::from("Hello there crab");
         let s_ref = &s; // error: `s` does not live long enough
-        // Uncomment and see what happens
-        // s_ref
+                        // Uncomment and see what happens
+                        // s_ref
     };
 
     // 6. Count words with `take_reference_and_count()`
-    
+    let num_words = take_reference_and_count(&s);
     println!("{} has {} words", s, num_words);
 
     // 8. Call `remove_first_word()` with a mutable reference to `s`
